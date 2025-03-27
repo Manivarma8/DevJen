4a22eb8b2c935cbf729a57d7827d7d72b79d03d1
Manivarma8 <maniversegit@gmail.com>
2025-03-27 17:43:49 +0530

    second

--- src/main/java/com/devjen/control/ControlJen.java
+++ src/main/java/com/devjen/control/ControlJen.java
@@ -1,6 +1,7 @@
 package com.devjen.control;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class ControlJen {
 	@GetMapping(value = "/get")
 	public String getVal() {
 		return "Move Ahead";
+	}
+	
+	@GetMapping(value = "getAll/{a}")
+	public String getDin(@PathVariable String a) {
+		return "My name is " + a;
 	}
 }
